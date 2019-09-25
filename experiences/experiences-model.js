9ad8369daefd0abcf3f77e7ca7939679da188049
Randy Warrington <warrington.randy@outlook.com>
2019-09-24 22:12:03 -0400

    added experience model and router files

--- experiences/experiences-model.js
+++ experiences/experiences-model.js
@@ -4,5 +4,29 @@ module.exports = {
     add,
     find,
     findby,
-    
+    findbyid,
+};
+
+function find() {
+    return db('experiences').select('category');
+}
+
+function findby(filter) {
+    return db('experiences').where(filter);
+}
+
+function add(experiences) {
+    return db('experiences')
+      .insert(user)
+      .then(ids => {
+        const [id] = ids;
+        console.log('ids', id)
+        return findById(id);
+      });
+}
+
+function findById(id) {
+    return db('experiences')
+    .where({id})
+    .first()
 }
